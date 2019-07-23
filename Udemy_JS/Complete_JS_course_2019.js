2953e85822e68097ec7df6930703c41d7df8ab41
João Henrique Xavier <jhcxavier@gmail.com>
2019-07-23 18:27:54 +0000

    updates

--- Udemy_JS/Complete_JS_course_2019.js
+++ Udemy_JS/Complete_JS_course_2019.js
@@ -258,3 +258,4 @@ switch(true){
         console.log('draw')
         break;
 }
+//Here would be Functions, however in the folder LinkdIn there's a file called Functions that I dedicated exclusively for this topic!!
