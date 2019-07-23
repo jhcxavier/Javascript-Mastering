c8e9ac7c5c0a97c04a4c32f6241ed8a2dc10307b
João Henrique Xavier <jhcxavier@gmail.com>
2019-07-23 15:44:04 +0000

    udemy course added

---
+++ Udemy_JS/Complete_JS_course_2019.js
@@ -0,0 +1,126 @@
+/*****************************
+* Variables and data types
+*/
+/*
+var firstName = 'John';
+console.log(firstName);
+var lastName = 'Smith';
+var age = 28;
+var fullAge = true;
+console.log(fullAge);
+var job;
+console.log(job);
+job = 'Teacher';
+console.log(job);
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and MArk';
+var if = 23;
+*/
+
+
+
+/*****************************
+* Variable mutation and type coercion
+*/
+/*
+var firstName = 'John';
+var age = 28;
+// Type coercion
+console.log(firstName + ' ' + age);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+
+/*****************************
+* Basic operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+// Math operators
+yearJohn = now - ageJohn;
+yeahMark = now - ageMark;
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older tha John');
+var x;
+console.log(typeof x);
+*/
+
+
+
+/*****************************
+* Operator precedence
+*/
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+// More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x--;
+console.log(x);
+*/
+
+
+
+/*****************************
+* CODING CHALLENGE 1
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
+*/
+
+let massMark = 80 //kg
+let heighMark = 1.80 //meters
+
+let massJohn = 90, heighJohn = 1.95;
+let bmiMark = massMark/heighMark^2;
+let bmiJohn = massJohn/heighJohn^2;
+if (bmiJohn > bmiMark){
+    console.log('Bmi John is bigger than Bmi Mark')
+}
+else console.log('Bmi Mark is bigger than John')
+//####################################################################
+
