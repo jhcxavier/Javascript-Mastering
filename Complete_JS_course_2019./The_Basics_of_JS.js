7b9319ece78ce724cf53b9f121b1d7559d5abf08
João Henrique Xavier <jhcxavier@gmail.com>
2019-07-25 20:06:02 +0000

    challange

--- Complete_JS_course_2019./The_Basics_of_JS.js
+++ Complete_JS_course_2019./The_Basics_of_JS.js
@@ -370,9 +370,91 @@ let tips = [tipCalulator(bill[0]),tipCalulator(bill[1]), tipCalulator(bill[2])]
 
 console.log(tips)
 
+/*****************************
+* Objects and properties
+*/
+    let john = {
+        firstName: 'John',
+        lastName: 'Smith',
+        birthYear: 1990,
+        family: ['Jane', 'Mark', 'Bob', 'Emily'],
+        job: 'teacher',
+        isMarried: false
+    };
+
+    console.log(john.firstName)
+    console.log(john['lastName'])
+    let x = "birthYear";
+    console.log(john[x])
+
+    john.job = 'designer'
+
+// new Object syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
 
 
-git
+/*****************************
+* Objects and methods
+*/
+
+let john = {
+        firstName: 'John',
+        lastName: 'Smith',
+        birthYear: 1992,
+        family: ['Jane', 'Mark', 'Bob', 'Emily'],
+        job: 'teacher',
+        isMarried: false,
+        calcAge: function(birthYear){
+            this.age = 2018 - this.birthYear;
+        }
+    };
+john.calcAge()
+
+
+    console.log(john);
+
+/*****************************
+* CODING CHALLENGE 4
+*/
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+GOOD LUCK
+*/
+
+let john = {
+    fullName : 'John Smith',
+    mass: 80,
+    height: 1.80,
+    calcBmi: function(){
+        this.bmi = this.mass / this.height^2
+    }
+}
+john.calcBmi()
+
+let mark = {
+    fullName : 'Mark Smith',
+    mass: 85,
+    height: 1.75,
+    calcBmi: function(){
+        this.bmi = this.mass / this.height^2
+    }
+}
+mark.calcBmi()
+
+if (john.bmi < mark.bmi){
+    console.log(john.fullName + ' has the biggest bmi of '+john.bmi)
+}else {
+    console.log(mark.fullName + ' has the biggest bmi of '+mark.bmi)
+}
 
 
 
