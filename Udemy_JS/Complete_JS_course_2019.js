e979f29583098a466f0e19d72d662d71e93b05bf
João Henrique Xavier <jhcxavier@gmail.com>
2019-07-25 01:48:52 +0000

    challenge complete

--- Udemy_JS/Complete_JS_course_2019.js
+++ Udemy_JS/Complete_JS_course_2019.js
@@ -340,3 +340,39 @@ let isDesigner = john.indexOf('teacher') === 3 ? 'John is a teacher' : 'Is not a
 console.log(isDesigner)
 
 
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator (as a function).
+He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+*/
+
+let tipCalulator = function(bill){
+
+    if(bill < 50){
+        tip = bill * 0.2
+    }else if (bill < 200){
+        tip = bill * 0.15
+    }else {
+        tip = bill *0.1
+    }
+    return tip + bill;
+}
+let bill = [124, 48, 268];
+let tips = [tipCalulator(bill[0]),tipCalulator(bill[1]), tipCalulator(bill[2])]
+
+console.log(tips)
+
+
+
+
+
+
+
