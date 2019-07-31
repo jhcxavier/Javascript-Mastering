2049d1bf6437d542e9d5df831395b7f5b553f398
João Henrique Xavier <jhcxavier@gmail.com>
2019-07-31 03:30:10 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -242,3 +242,18 @@ function isOdd(num) {
   }
 }
 console.log(isOdd(10))
+
+//=================================================================================
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+//  Input will always be valid, i.e. no negative integers.
+
+
+
+var countSheep = function (num){
+  let sum = "";
+  for (let i = 0; i<num; i++){
+  sum = sum + (i + 1) + " sheep..."
+  }
+  return sum;
+}
+countSheep(5);
