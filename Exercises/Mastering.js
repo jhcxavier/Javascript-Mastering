a9fb53509e4c3590f0428c408c43ea83110b2c7b
João Henrique Xavier <jhcxavier@gmail.com>
2019-08-07 03:42:20 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -319,7 +319,22 @@ console.log(isEitherEven(1, 5))
 //-----------------------------------------------------------------------------------------------
 
 
+//  Write a function called "isOddLength".
 
+// Given a word, "isOddLength" returns whether the length of the given word is odd.
+
+// var output = isOddLength('special');
+// console.log(output); // --> true
+function isOddLength(word) {
+  // your code here
+  if (word.length % 2 !== 0){
+    return true
+  }else {
+    return false;
+  }
+
+}
+console.log(isOddLength('special'))
 
 
 
