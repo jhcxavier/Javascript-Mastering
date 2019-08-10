2208833ca55af282871fa9a6afb7cdfe014ac016
João Henrique Xavier <jhcxavier@gmail.com>
2019-08-10 03:32:01 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -335,7 +335,38 @@ function isOddLength(word) {
 
 }
 console.log(isOddLength('special'))
+//_______________________________________________________________
+//  Write a function called "isEvenLength".
 
+// Given a word, "isEvenLength" returns whether the length of the word is even.
 
+// var output = isEvenLength('wow');
+// console.log(output); // --> false
+function isEvenLength(word) {
+  // your code here
+  if(word.length % 2 ==0){
+    return true;
+  }else {
+    return false;
+  }
+}
+console.log(isEvenLength('wow'))
+
+//___________________________________________________________________
+// Write a function called "isEvenAndGreaterThanTen".
+
+// Given a number, "isEvenAndGreaterThanTen" returns whether it is both even and greater than 10.
+
+// var output = isEvenAndGreaterThanTen(13);
+// console.log(output); // --> false
+function isEvenAndGreaterThanTen(num) {
+  // your code here
+  if(num > 10 && num % 2 === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log(isEvenAndGreaterThanTen(13))
 
 
