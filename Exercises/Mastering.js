398330c0a1d5c14dedd47da70e4f82dbf0ebb020
João Henrique Xavier <jhcxavier@gmail.com>
2019-08-12 01:25:01 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -382,3 +382,15 @@ function average(num1, num2) {
 }
 console.log(average(5, 2))
 
+//_______________________________________________________________
+//  Write a function called "computeAreaOfATriangle".
+
+// Given the base and height of a triangle, "computeAreaOfATriangle" returns its area.
+
+// var output = computeAreaOfATriangle(4, 6);
+// console.log(output); // --> 12
+function computeAreaOfATriangle(base, height) {
+  // your code here
+  return (base * height) / 2
+}
+console.log(computeAreaOfATriangle(4,6))
