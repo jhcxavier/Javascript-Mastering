04d4ad908f94ae5256f61c4982148b8b1a20ac66
João Henrique Xavier <jhcxavier@gmail.com>
2019-08-13 21:09:05 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -407,8 +407,18 @@ function cube(num) {
   return num*num*num;
 }
 console.log(cube(3))
+//------------------------------------------________----------------
+//  Write a function called "square".
 
+// Given a number, "square" should return the square of the given number.
 
+// var output = square(5);
+// console.log(output); // --> 25
+function square(num) {
+  // your code here
+  return num*num;
+}
+console.log(square(5))
 
 
 
