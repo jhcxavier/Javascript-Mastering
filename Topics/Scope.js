f5e88522bd874903e7f401682f69171f58814e60
João Henrique Xavier <jhcxavier@gmail.com>
2019-08-19 14:52:41 +0000

    JavaScript scope

---
+++ Topics/Scope.js
@@ -0,0 +1,35 @@
+//'use strict';
+
+const warrior = {
+    name: 'Jujin Take',
+    type: 'Ninja',
+    weapon: 'Shuriken',
+    agility: 79
+} //this is globally scoped
+
+//globally scope
+const screamWarrior = () => {
+    let warrior2 = 'Samurai';
+    warrior3 = 'Viking';
+
+
+    warrior3 = 'Viking'; // if do not add const, var or let the variable will be globally scope.
+    return {
+        shootWarrior: () => {
+            return console.log(warrior, warrior2);
+        }
+    }
+}
+const newWarrior = screamWarrior();
+
+newWarrior.shootWarrior();
+
+console.log(warrior, warrior3)
+
+
+
+// var warrior; // warrior is declared here...
+
+
+// var warrior3;//will show undefined
+
