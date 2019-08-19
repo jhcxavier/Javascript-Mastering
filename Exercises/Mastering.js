33fbf036edbb4c9271b4cda0a9074e8d8da9006c
João Henrique Xavier <jhcxavier@gmail.com>
2019-08-19 00:28:27 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -419,8 +419,19 @@ function square(num) {
   return num*num;
 }
 console.log(square(5))
+//_________________________________________________
 
+// Write a function called "computeAverageLengthOfWords".
 
+// Given two words, "computeAverageLengthOfWords" returns the average of their lengths.
+
+// var output = computeAverageLengthOfWords('code', 'programs');
+// console.log(output); // --> 6
+function computeAverageLengthOfWords(word1, word2) {
+
+  return (word1.length + word2.length)/2
+}
+console.log(computeAverageLengthOfWords('code', 'programs'))
 
 
 
