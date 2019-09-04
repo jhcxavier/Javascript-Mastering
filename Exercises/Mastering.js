3d05d6addad56351a425b662a22c58da7911ab6a
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2019-09-04 14:05:45 -0300

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -432,9 +432,22 @@ function computeAverageLengthOfWords(word1, word2) {
   return (word1.length + word2.length)/2
 }
 console.log(computeAverageLengthOfWords('code', 'programs'))
+_______________________________________________
 
+//  Write a function called "addFullNameProperty".
 
+// Given an object that has a "firstName" property and a "lastName" property, "addFullNameProperty" returns a "fullName" property whose value is a string with the first name and last name separated by a space.
 
+// var person = {
+//   firstName: 'Jade',
+//   lastName: 'Smith'
+// };
+// addFullNameProperty(person);
+// console.log(person.fullName); // --> 'Jade Smith'
 
+function addFullNameProperty(obj) {
+  
+  obj.fullName = obj.firstName + " " + obj.lastName;
+}
 
 
