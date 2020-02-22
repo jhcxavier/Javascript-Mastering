aa0d3cad85fb91a84e2fd061b2a56f0b6e6b68a2
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-22 22:33:31 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -523,3 +523,14 @@ function merge(a, b) {
   return m;
 }
 merge(array1, array2)
+//___________________________________________________________________________
+//  Write a function called "addFullNameProperty".
+
+// Given an object that has a "firstName" property and a "lastName" property, 
+// "addFullNameProperty" returns a "fullName" property whose value is a string 
+// with the first name and last name separated by a space.
+function addFullNameProperty(obj) {
+
+  obj.fullName = obj.firstName + " " + obj.lastName;
+}
+//___________________________________________________________________________
