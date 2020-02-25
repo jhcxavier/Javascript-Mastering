2e4255b67caa2feb5c8540b0a1ebbf6bf7b8b7a1
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-25 21:17:28 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -590,3 +590,13 @@ function isPersonOldEnoughToVote(obj) {
   }
 }
 console.log(isPersonOldEnoughToVote(17))
+//___________________________________________________________________________
+// Write a function called "getNthElement".
+// Given an array and an integer, "getNthElement" 
+// returns the element at the given integer, within the given array.
+// Notes:* If the array has a length of 0, it should return 'undefined'.
+
+function getNthElement(array, n) {
+  return array[n];
+}
+//___________________________________________________________________________
