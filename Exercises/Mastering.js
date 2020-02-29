e434535d4417a68fbb702d09d337864628e7ab1b
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-29 05:42:12 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -621,3 +621,17 @@ function getFirstElement(array) {
 function getLastElement(array) {
 return array.pop();
 }
+//___________________________________________________________________________
+//  Write a function called "addToFront".
+
+// Given an array and an element, "addToFront" adds the given element to 
+// the front of the given array, and returns the given array.
+
+// Notes: * It should be the SAME array, not a new array.
+
+// var output = addToFront([1, 2], 3);
+// console.log(output); // -> [3, 1, 2]
+function addToFront(arr, element) {
+  arr.unshift(element)
+  return arr
+}
