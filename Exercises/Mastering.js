42765c9312e09cd1524c3865417568e40a3b03ca
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-29 05:40:21 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -610,3 +610,14 @@ function getNthElement(array, n) {
 function getFirstElement(array) {
   return array.shift()
 }
+//___________________________________________________________________________
+// Write a function called "getLastElement".
+
+// Given an array, "getLastElement" returns the last element of the given array.
+// Notes:* If the given array has a length of 0, it should return 'undefined'.
+
+// var output = getLastElement([1, 2, 3, 4]);
+// console.log(output); // --> 4
+function getLastElement(array) {
+return array.pop();
+}
