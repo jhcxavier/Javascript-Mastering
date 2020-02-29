d0e7fb31feb74f9f8151c30a1698bb04e9d3ba8e
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-29 05:38:59 +0000

    update

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -600,3 +600,13 @@ function getNthElement(array, n) {
   return array[n];
 }
 //___________________________________________________________________________
+// Write a function called "getFirstElement".
+
+// Given an array, "getFirstElement" returns the first element of the given array.
+// Notes:* If the given array has a length of 0, it should return undefined.
+
+// var output = getFirstElement([1, 2, 3, 4, 5]);
+// console.log(output); // --> 1
+function getFirstElement(array) {
+  return array.shift()
+}
