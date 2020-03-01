1b7daae33a655dea4a3b5b9de078a7d212b4a167
João Henrique Xavier <jhcxavier@gmail.com>
2020-03-01 10:49:41 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -635,3 +635,14 @@ function addToFront(arr, element) {
   arr.unshift(element)
   return arr
 }
+
+//  Write a function called "addToBack".
+
+// Given an array and an element, "addToBack" returns 
+// the given array with the given element added to the end.
+
+// Note: It should be the SAME array, not a new array.
+function addToBack(arr, element) {
+  arr.push(element)
+  return arr;
+}
