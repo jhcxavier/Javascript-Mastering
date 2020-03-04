dfc0ab9d23e47fe8bf87c7ddfeed2f99b1721ac0
João Henrique Xavier <jhcxavier@gmail.com>
2020-03-04 12:41:39 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -699,3 +699,13 @@ function doubleSquareRootOf(num) {
   return Math.sqrt(num)*2;
 }
 doubleSquareRootOf(9)
+//___________________________________________________________________________
+
+// Write a function called "getLengthOfThreeWords".
+
+// Given 3 words, "getLengthOfThreeWords" returns the sum of their lengths.
+function getLengthOfThreeWords(word1, word2, word3) {
+  // your code here
+  return word1.length + word2.length + word3.length;
+}
+console.log(getLengthOfThreeWords());
