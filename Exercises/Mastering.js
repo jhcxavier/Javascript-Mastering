f9297fd085a41a9dbb611f93a613590954445e75
João Henrique Xavier <jhcxavier@gmail.com>
2020-03-05 10:09:26 +0000

    updates

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -709,3 +709,18 @@ function getLengthOfThreeWords(word1, word2, word3) {
   return word1.length + word2.length + word3.length;
 }
 console.log(getLengthOfThreeWords());
+//___________________________________________________________________________
+// Write a function called "joinArrays".
+
+// Given two arrays, "joinArrays" returns an array with the elements of "arr1" in order, followed by the elements in "arr2". 
+
+// var output = joinArrays([1, 2], [3, 4]);
+// console.log(output); // --> [1, 2, 3, 4]
+
+// You should be familiar with the "concat" method for this problem. 
+function joinArrays(arr1, arr2) {
+    
+    return arr1.concat(arr2)
+
+}
+console.log(joinArrays())
