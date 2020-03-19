3018f13a5f279b6b6e97260ccfabb1078faaf8d7
João Henrique Xavier <jhcxavier@gmail.com>
2020-03-19 17:05:14 +0000

    constructor pattern

---
+++ JavaScript Patterns/PrototypeOrClassDesignPattern.js
@@ -0,0 +1,18 @@
+class Car {
+    constructor(doors, engine, color){
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
+class Suv extends Car{
+    constructor(doors, engine, color){
+        super(doors, engine, color);
+        this.lights = "led";
+    }
+}
+const civic = new Car(4, "V6", "White")
+const cx5 = new Suv(4, "V8", "grey")
+
+console.log(cx5);
+console.log(civic);
