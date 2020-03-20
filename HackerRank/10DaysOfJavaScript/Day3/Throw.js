14c582b67ae34202b673d7653ac9f66924f16706
João Henrique Xavier <jhcxavier@gmail.com>
2020-03-20 20:03:44 +0000

    Throw

---
+++ HackerRank/10DaysOfJavaScript/Day3/Throw.js
@@ -0,0 +1,9 @@
+// In this challenge, we practice using throw and catch statements to work with custom error messages.
+
+// Task
+
+// Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. 
+// Otherwise, it must throw an Error according to the following rules:
+
+// If a is 0, throw an Error with message= Zero Error.
+// If a is negative, throw an Error with message= Negative Error.
