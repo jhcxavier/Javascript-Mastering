9375f1ef8c024bd4936a8a007cc0ed880a27b447
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-03-27 17:18:32 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -782,3 +782,16 @@ function getAllElementsButLast(array) {
  return array
 }
 getAllElementsButLast(arr) 
+//___________________________________________________________________________
+// Write a function called "removeFromFront".
+
+// Given an array, "removeFromFront" returns the given array with its first element removed.
+// Notes:* You should be familiar with the method 'shift'.
+
+// var output = removeFromFront([1, 2, 3]);
+// console.log(output); // --> [2, 3]
+
+function removeFromFront(arr) {
+  arr.shift()
+  return arr;
+}
