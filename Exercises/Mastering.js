c31a25b93c2754986fc232506bc06d37d3697a9f
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-03-30 20:26:45 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -815,3 +815,20 @@ function removeFromBackOfNew(arr) {
 }
 removeFromBackOfNew(array)
 
+//___________________________________________________________________________
+// Write a function called "removeFromFrontOfNew".
+
+// Given an array, "removeFromFrontOfNew" returns a new array containing all but the first element of the given array.
+
+// Notes:* You should be familiar with the 'slice' method.
+
+// var arr = [1, 2, 3];
+// var output = removeFromFrontOfNew(arr);
+// console.log(output); // --> [2, 3]
+// console.log(arr); // --> [1, 2, 3]
+
+function removeFromFrontOfNew(arr) {
+  let temp = arr.slice(1,arr.length)
+  return temp;
+}
+removeFromFrontOfNew(arr)
