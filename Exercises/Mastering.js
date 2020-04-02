953d65c23f4388273f601c9790e395d77d53a34f
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-04-02 19:23:09 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -851,3 +851,15 @@ function countCharacter(str, char) {
   return result;
 }
 countCharacter(str, char);
+//___________________________________________________________________________
+// Write a function called "getAllLetters".
+
+// Given a word, "getAllLetters" returns an array containing every character in the word. 
+// Notes:* If given an empty string, it should return an empty array.
+
+// var output = getAllLetters('Radagast');
+// console.log(output); // --> ['R', 'a', 'd', 'a', 'g', 'a', 's', 't']
+function getAllLetters(str) {
+  // your code here
+ return  str.split("")
+}
