ee642d6b1c38762899a98a3cf1a6931271f3f426
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-04-08 18:05:04 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -894,3 +894,26 @@ function removeFromBack(arr) {
   arr.pop()
   return arr;
 }
+//___________________________________________________________________________
+//  Write a function called "isEitherEvenOrAreBoth7".
+
+// Given two numbers, 'isEitherEvenOrAreBoth7' returns whether at least one of them is even, or, both of them are 7.
+
+// var output = isEitherEvenOrAreBoth7(3, 7);
+// console.log(output); // --> false
+
+// var output = isEitherEvenOrAreBoth7(2, 3);
+// console.log(output); // --> true
+function isEitherEvenOrAreBoth7(num1, num2) {
+  // your code here
+  if(num1 % 2 == 0 || num2 % 2 == 0){
+    return true
+  }else if (num1 === 7 && num2 === 7){
+    return true
+  }else {
+    return false;
+  }
+}
+
+
+
