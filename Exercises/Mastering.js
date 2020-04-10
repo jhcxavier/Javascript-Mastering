6c366602330e58dd67d38a3f85de730442d50ed7
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-04-09 21:03:48 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -914,6 +914,27 @@ function isEitherEvenOrAreBoth7(num1, num2) {
     return false;
   }
 }
+//___________________________________________________________________________
+//  Write a function called "isEitherEvenAndLessThan9".
 
+// Given two numbers, 'isEitherEvenAndLessThan9' returns whether at least one of them is even, and, both of them are less than 9.
+
+// var output = isEitherEvenAndLessThan9(2, 4);
+// console.log(output); // --> true
+
+// var output = isEitherEvenAndLessThan9(72, 2);
+// console.log(output); // --> false
+function isEitherEvenAndLessThan9(num1, num2) {
+  if(num1 % 2 === 0 && num1 < 9 && num2 %2 !==0 && num2 < 7){
+    return true
+  }
+  else if (num2 % 2 === 0 && num2 < 9 && num1 %2 !==0 && num1 < 9)
+    return true;
+  else if (num1 % 2 === 0 && num1 < 9 && num2 %2==0 && num2 < 9)
+    return true;
+  else 
+    return false;
+}
+console.log(isEitherEvenAndLessThan9(8, 9))
 
 
