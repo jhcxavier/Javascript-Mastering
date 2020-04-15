ba5667745b0f7ff04c075bffbf8d8e45ced04c15
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-04-14 21:10:00 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -979,3 +979,34 @@ function extend(obj1, obj2) {
   return result;
 }
 console.log(extend(obj1, obj2))
+
+//-----------------------------------------
+// Write a function called "removeNumbersLargerThan".
+
+// Given a number and an object, "removeNumbersLargerThan" 
+// removes any properties whose values are numbers greater than the given number.
+
+// var obj = {
+//   a: 8,
+//   b: 2,
+//   c: 'montana'
+// }
+// removeNumbersLargerThan(5, obj);
+// console.log(obj); // --> { b: 2, c: 'montana' }
+var obj = {
+  a: 8,
+  b: 2,
+  c: 'montana'
+}
+let num = 5;
+function removeNumbersLargerThan(num, obj) {
+  // your code here
+  console.log(num)
+  for(let [key, value] of Object.entries(obj)){
+    if(value !== String && value > num){
+      delete obj[key]
+    }
+  }
+  return obj
+}
+removeNumbersLargerThan(num, obj)
