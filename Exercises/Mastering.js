fb9192632380c895c5421ee3cbf843e91eea7b52
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-04-15 21:45:43 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -1010,3 +1010,24 @@ function removeNumbersLargerThan(num, obj) {
   return obj
 }
 removeNumbersLargerThan(num, obj)
+//-----------------------------------------------
+// Write a function called "removeNumbersLessThan".
+
+// Given a number and an object, "removeNumbersLessThan" 
+// removes any properties whose values are numbers less than the given number.
+
+// var obj = {
+//   a: 8,
+//   b: 2,
+//   c: 'montana'
+// }
+// removeNumbersLessThan(5, obj);
+// console.log(obj); // --> { a: 8, c: 'montana' }
+function removeNumbersLessThan(num, obj) {
+  Object.entries(obj).forEach(([key, value])=>{
+    if(value !== String && value < num){
+      delete obj[key]
+    }
+  })
+  return obj
+}
