debc38bcc71180b9dfabf10c7ecff09c5648e8f1
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-05-01 20:34:55 -0400

    Update Mastering.js

--- Exercises/Mastering.js
+++ Exercises/Mastering.js
@@ -1136,3 +1136,31 @@ function removeOddValues(obj) {
   return obj
 }
 removeOddValues(obj)
+//------------------------------------------
+// Write a function called "removeArrayValues".
+
+// Given an object, "removeArrayValues" removes any properties whose values are arrays.
+
+// var obj = {
+//   a: [1, 3, 4],
+//   b: 2,
+//   c: ['hi', 'there']
+// }
+// removeArrayValues(obj);
+// console.log(obj); // --> { b: 2 }
+function removeArrayValues(obj) {
+  
+  // your code here
+  for(let [key, value] of Object.entries(obj)){
+    // let array = Array.isArray(value)
+    if(Array.isArray(value)==true){
+      delete obj[key]
+    }
+  }
+  return obj;
+}
+removeArrayValues({
+  a: [1, 3, 4],
+  b: 2,
+  c: ['hi', 'there']
+})
