0cc500f67d8c57a8bdd34b17c62d6a07b7bc5ce3
João Henrique Xavier <jhcxavier@gmail.com>
2021-05-09 21:55:34 +0000

    get method hash table

--- DataStructures/HashTable.js
+++ DataStructures/HashTable.js
@@ -17,14 +17,19 @@ HashTable.prototype.hash = function(key){
     return total % this.numBuckets;
 }
 HashTable.prototype.insert = function(key, value){
-
     let index = this.hash(key);
     console.log("index: ",index)
     if(!this.buckets[index]){
         this.buckets[index] = new HashNode(key, value)
+    }else if(this.buckets[index].key === key){
+        this.buckets[index].value = value;
     }else{
         let currentNode = this.buckets[index];
         while(currentNode.next){
+            if(currentNode.next.key === key){
+                currentNode.next.value = value;
+                return
+            }
             currentNode = currentNode.next
         }
         currentNode.next = new HashNode(key, value)
@@ -32,9 +37,27 @@ HashTable.prototype.insert = function(key, value){
 
 }
 
+HashTable.prototype.getMethod=function(key){
+    let index = this.hash(key)
+    if(!this.buckets[index]){
+        return null
+    }else{
+        let currentNode = this.buckets[index]
+        while(currentNode){
+              if(currentNode.key === key){
+                return currentNode.value;
+            }
+            currentNode = currentNode.next
+        }
+        return null
+    }
+}
 let myHT = new HashTable(30)
 
 myHT.insert("Dean", "dean@gmail.com")
 myHT.insert("Joe", "joe@gmail.com")
 myHT.insert("Smith", "smith@gmail.com")
 console.log(myHT.buckets)
+myHT.insert("Smith", "smith111@gmail.com")
+myHT.insert("Joe", "joe1@aol.com")
+console.log(myHT.getMethod("Joe"))
