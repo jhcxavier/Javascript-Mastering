2a17c6922a52ee3f36766cc921c06bca3887fc2e
João Henrique Xavier <jhcxavier@gmail.com>
2021-05-09 23:13:36 +0000

    adding harmlessRansomNote algorithm

--- Algorithms/fizzBuzz.js
+++ Algorithms/fizzBuzz.js
@@ -1,6 +1,5 @@
 //Recursion
 function fizzBuzz(num, index){
-    
     if(index % 15 === 0) console.log("FizzBuzz")
     else if(index % 3 === 0) console.log("Fizz")
     else if(index % 5 === 0) console.log("Buzz")
